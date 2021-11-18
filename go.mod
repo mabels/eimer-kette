502b78605abb8f2deb07094be4786fbbda780cae
Meno Abels <meno.abels@adviser.com>
2021-11-18 10:09:04 +0100

    chore: cleanup and args

--- go.mod
+++ go.mod
@@ -24,8 +24,11 @@ require (
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/gammazero/deque v0.1.0 // indirect
 	github.com/gammazero/workerpool v1.1.2 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/reactivex/rxgo/v2 v2.5.0 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	github.com/teivah/onecontext v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,172 +1,68 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX - License - Identifier: Apache - 2.0
 package main
 
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path"
+	"strings"
 	"sync/atomic"
 
-	// "github.com/alitto/pond"
+	"github.com/alitto/pond"
+	"github.com/spf13/cobra"
 
-	"github.com/gammazero/workerpool"
-	// "github.com/alitto/pond"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
-// S3ListObjectsAPI defines the interface for the ListObjectsV2 function.
-// We use this interface to test the function using a mocked service.
-// type S3ListObjectsAPI interface {
-// ListObjectsV2(ctx context.Context,
-// params *s3.ListObjectsV2Input,
-// optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error)
-// }
-
-// GetObjects retrieves the objects in an Amazon Simple Storage Service (Amazon S3) bucket
-// Inputs:
-//     c is the context of the method call, which includes the AWS Region
-//     api is the interface that defines the method call
-//     input defines the input arguments to the service call.
-// Output:
-//     If success, a ListObjectsV2Output object containing the result of the service call and nil
-//     Otherwise, nil and an error from the call to ListObjectsV2
-// func GetObjects(c context.Context, api S3ListObjectsAPI, input *s3.ListObjectsV2Input) (*s3.ListObjectsV2Output, error) {
-// }
-
 type Config struct {
-	prefix    string
-	delimiter string
-	bucket    string
-	aws       aws.Config
-	maxKeys   int32
-	s3Worker  int
-	outWorker int
+	gitCommit     string
+	version       string
+	prefix        *string
+	delimiter     *string
+	format        *string
+	bucket        *string
+	region        *string
+	maxKeys       *int32
+	s3Workers     *int
+	outWorkers    *int
+	statsFragment *uint64
 }
 
 type Calls struct {
-	newFromConfig uint
-	listObjectsV2 uint
+	newFromConfig      int64
+	listObjectsV2      int64
+	listObjectsV2Input int64
+}
+
+type TotalCurrent struct {
+	total      Calls
+	concurrent Calls
 }
 
 type Channels struct {
-	calls    Calls
+	calls    TotalCurrent
 	channels chan *s3.Client
+}
+
+type Output struct {
+	fileStream io.Writer
 }
 
 type S3StreamingLister struct {
 	config          Config
 	inputConcurrent int32
 	clients         Channels
+	output          Output
+	aws             aws.Config
 }
 
 type RunStatus struct {
 	outObjects uint64
-}
-
-func outWriter(tos Complete, chstatus chan RunStatus) {
-	chstatus <- RunStatus{outObjects: uint64(len(tos.todo))}
-	for _, item := range tos.todo {
-		out, _ := json.Marshal(item)
-		fmt.Println(string(out))
-	}
-}
-
-func s3Lister(input s3.ListObjectsV2Input, chi chan *s3.ListObjectsV2Input, cho chan Complete, app *S3StreamingLister) {
-	var client *s3.Client
-	// newFromConfig := false
-	select {
-	case x := <-app.clients.channels:
-		client = x
-	default:
-		// newFromConfig = true
-		app.clients.calls.newFromConfig += 1
-		client = s3.NewFromConfig(app.config.aws)
-	}
-	// chx := observable.Observe()
-	// fmt.Println("hallo")
-	// input := item.V.(*s3.ListObjectsV2Input)
-	// if input.Prefix != nil {
-	// 	fmt.Fprintln(os.Stderr, "input bucket=", *input.Bucket, " newFromConfig=", newFromConfig, " next=", input.ContinuationToken, " prefix=", *input.Prefix)
-	// } else {
-	// 	fmt.Fprintln(os.Stderr, "input bucket=", *input.Bucket, " newFromConfig=", newFromConfig, " next=", input.ContinuationToken)
-	// }
-	// fmt.Fprintln(os.Stderr, "client=", client)
-	app.clients.calls.listObjectsV2 += 1
-	resp, err := client.ListObjectsV2(context.TODO(), &input)
-	if err != nil {
-		fmt.Println("Got error retrieving list of objects:")
-		fmt.Println(err)
-		return
-	}
-	// fmt.Fprintln(os.Stderr, "-- list objects v2")
-	app.clients.channels <- client
-	// fmt.Fprintln(os.Stderr, "-- pushed back to clients")
-	// paging
-	if resp.NextContinuationToken != nil {
-		// fmt.Println("next", resp.NextContinuationToken)
-		// go func() {
-		// fmt.Fprintln(os.Stderr, "-- pre next")
-		// fmt.Fprintln(os.Stderr, "Prefix=", *input.Prefix, " next=", resp.NextContinuationToken)
-		chi <- &s3.ListObjectsV2Input{
-			MaxKeys:           app.config.maxKeys,
-			Delimiter:         input.Delimiter,
-			Prefix:            input.Prefix,
-			ContinuationToken: resp.NextContinuationToken,
-			Bucket:            input.Bucket,
-		}
-		// fmt.Fprintln(os.Stderr, "-- post next")
-		// }()
-	} else {
-		atomic.AddInt32(&app.inputConcurrent, -1)
-	}
-	// out := ""
-	// if input.Prefix != nil {
-	// out = *input.Prefix
-	// }
-	// fmt.Println("prefix=", out, "len=", len(resp.Contents), " next=", resp.NextContinuationToken, " res=", *resp.Contents[0].Key)
-	// sub prefixe
-	atomic.AddInt32(&app.inputConcurrent, int32(len(resp.CommonPrefixes)))
-	// go func() {
-	// fmt.Fprintln(os.Stderr, "-- pre prefixed=%d", len(resp.CommonPrefixes))
-	for _, item := range resp.CommonPrefixes {
-		// fmt.Fprintln(os.Stderr, "Prefix=", *item.Prefix)
-		chi <- &s3.ListObjectsV2Input{
-			MaxKeys:   app.config.maxKeys,
-			Delimiter: &app.config.delimiter,
-			Prefix:    item.Prefix,
-			Bucket:    &app.config.bucket,
-		}
-	}
-	// fmt.Fprintln(os.Stderr, "-- post prefixed contents=%d", len(resp.Contents))
-	// }()
-	// output of files
-	// go func() {
-	// fmt.Fprintln(os.Stderr, "Contents:", len(resp.Contents))
-	cho <- Complete{todo: resp.Contents, completed: false}
-	// fmt.Fprintln(os.Stderr, "-- post contents")
-	if app.inputConcurrent == 0 {
-		fmt.Fprintln(os.Stderr, "Stop-Concurrent")
-		cho <- Complete{todo: nil, completed: true}
-	}
-	// }()
-
-	// fmt.Println("Objects in " + *bucket)
-	// for _, item := range resp.Contents {
-	// 	fmt.Println("Name:          ", *item.Key)
-	// 	//	fmt.Println("Last modified: ", *item.LastModified)
-	// 	//	fmt.Println("Size:          ", item.Size)
-	// 	//	fmt.Println("Storage class: ", item.StorageClass)
-	// 	//	fmt.Println("")
-	// }
-
-	// fmt.Println("Found", len(resp.Contents), "items in bucket", *bucket, " next=", resp.NextContinuationToken)
-	// fmt.Println("")
 }
 
 type Complete struct {
@@ -174,58 +70,154 @@ type Complete struct {
 	todo      []types.Object
 }
 
-func main() {
-	bucket := flag.String("b", "", "The name of the bucket")
-	flag.Parse()
+func outWriter(app S3StreamingLister, tos Complete, chstatus chan RunStatus) {
+	chstatus <- RunStatus{outObjects: uint64(len(tos.todo))}
+	for _, item := range tos.todo {
+		if *app.config.format == "mjson" {
+			// Add BucketName
+			// app.config.bucket
+			out, _ := json.Marshal(item)
+			fmt.Fprintln(app.output.fileStream, string(out))
+		} else if *app.config.format == "sqs" {
+			panic("not yet sqs")
+			// out, _ := json.Marshal(item)
+		}
+	}
+}
 
-	if *bucket == "" {
-		fmt.Println("You must supply the name of a bucket (-b BUCKET)")
+func s3Lister(input s3.ListObjectsV2Input, chi chan *s3.ListObjectsV2Input, cho chan Complete, app *S3StreamingLister) {
+	var client *s3.Client
+	atomic.AddInt64(&app.clients.calls.concurrent.newFromConfig, 1)
+	select {
+	case x := <-app.clients.channels:
+		client = x
+	default:
+		atomic.AddInt64(&app.clients.calls.total.newFromConfig, 1)
+		client = s3.NewFromConfig(app.aws)
+	}
+	atomic.AddInt64(&app.clients.calls.concurrent.newFromConfig, -1)
+	atomic.AddInt64(&app.clients.calls.total.listObjectsV2, 1)
+	atomic.AddInt64(&app.clients.calls.concurrent.listObjectsV2, 1)
+	resp, err := client.ListObjectsV2(context.TODO(), &input)
+	if err != nil {
+		fmt.Println("Got error retrieving list of objects:")
+		fmt.Println(err)
 		return
 	}
+	app.clients.channels <- client
+	atomic.AddInt64(&app.clients.calls.concurrent.listObjectsV2Input, -1)
+	atomic.AddInt64(&app.clients.calls.concurrent.listObjectsV2, -1)
 
-	cfg, err := config.LoadDefaultConfig(context.TODO())
-	if err != nil {
-		panic("configuration error, " + err.Error())
+	if resp.NextContinuationToken != nil {
+		atomic.AddInt64(&app.clients.calls.total.listObjectsV2Input, 1)
+		atomic.AddInt64(&app.clients.calls.concurrent.listObjectsV2Input, 1)
+		chi <- &s3.ListObjectsV2Input{
+			MaxKeys:           *app.config.maxKeys,
+			Delimiter:         input.Delimiter,
+			Prefix:            input.Prefix,
+			ContinuationToken: resp.NextContinuationToken,
+			Bucket:            input.Bucket,
+		}
+	} else {
+		atomic.AddInt32(&app.inputConcurrent, -1)
 	}
 
-	// observable := rxgo.Defer([]rxgo.Producer{func(_ context.Context, ch chan<- rxgo.Item) {
-	// 	for i := 0; i < 3; i++ {
-	// 		ch <- rxgo.Of(i)
-	// 	}
-	// }})
+	atomic.AddInt32(&app.inputConcurrent, int32(len(resp.CommonPrefixes)))
+	atomic.AddInt64(&app.clients.calls.total.listObjectsV2Input, int64(len(resp.CommonPrefixes)))
+	atomic.AddInt64(&app.clients.calls.concurrent.listObjectsV2Input, int64(len(resp.CommonPrefixes)))
+	for _, item := range resp.CommonPrefixes {
+		chi <- &s3.ListObjectsV2Input{
+			MaxKeys:   *app.config.maxKeys,
+			Delimiter: app.config.delimiter,
+			Prefix:    item.Prefix,
+			Bucket:    app.config.bucket,
+		}
+	}
+	cho <- Complete{todo: resp.Contents, completed: false}
+	if atomic.CompareAndSwapInt32(&app.inputConcurrent, 0, 0) {
+		fmt.Fprintln(os.Stderr, "Stop-Concurrent")
+		cho <- Complete{todo: nil, completed: true}
+	}
+}
 
-	// Submit 1000 tasks
-	// for i := 0; i < 1000; i++ {
-	// n := i
-	// pool.Submit(func() {
-	// fmt.Printf("Running task #%d\n", n)
-	// })
-	// }
+func versionStr(args *Config) string {
+	return fmt.Sprintf("Version: %s:%s\n", args.version, args.gitCommit)
+}
 
-	// Stop the pool and wait for all submitted tasks to complete
-	// pool.StopAndWait()
-	// cho := make(chan rxgo.Item)
+func parseArgs(app *S3StreamingLister, osArgs []string) {
+	rootCmd := &cobra.Command{
+		Use:     path.Base(osArgs[0]),
+		Short:   "s3-streaming-lister short help",
+		Long:    strings.TrimSpace("s3-streaming-lister long help"),
+		Version: versionStr(&app.config),
+		Args:    cobra.MinimumNArgs(0),
+		// RunE:         S3StreamingListerCmdE(&app.config),
+		SilenceUsage: true,
+	}
+	flags := rootCmd.PersistentFlags()
+	app.config.prefix = flags.String("prefix", *app.config.prefix, "aws prefix")
+	app.config.delimiter = flags.String("delimiter", *app.config.delimiter, "aws delimiter")
+	app.config.format = flags.String("format", *app.config.format, "mjson | sqs")
+	app.config.bucket = flags.String("bucket", *app.config.bucket, "aws bucket name")
+	app.config.region = flags.String("region", *app.config.region, "aws region name")
+	app.config.maxKeys = flags.Int32("maxKeys", *app.config.maxKeys, "aws maxKey pageElement size 1000")
+	app.config.s3Workers = flags.Int("s3Worker", *app.config.s3Workers, "number of query worker")
+	app.config.outWorkers = flags.Int("outWorkers", *app.config.outWorkers, "number of output worker")
+	app.config.statsFragment = flags.Uint64("statsFragment", *app.config.statsFragment, "number statistics output")
+	rootCmd.SetArgs(osArgs[1:])
+	rootCmd.Execute()
+}
 
-	s3Worker := 16
+func main() {
+	prefix := ""
+	delimiter := "/"
+	mjson := "mjson"
+	bucket := "unknown bucket"
+	region := "eu-central-1"
+	s3Workers := 16
+	outWorkers := 1
+	maxKeys := int32(1000)
+	statsFragment := uint64(10000)
 	app := S3StreamingLister{
 		config: Config{
-			prefix:    "",
-			delimiter: "/",
-			bucket:    *bucket,
-			aws:       cfg,
-			s3Worker:  s3Worker,
-			outWorker: 1,
-			maxKeys:   1000,
+			prefix:        &prefix,
+			delimiter:     &delimiter,
+			format:        &mjson,
+			bucket:        &bucket,
+			region:        &region,
+			s3Workers:     &s3Workers,
+			outWorkers:    &outWorkers,
+			maxKeys:       &maxKeys,
+			statsFragment: &statsFragment,
 		},
 		inputConcurrent: 0,
 		clients: Channels{
-			calls: Calls{
-				newFromConfig: 0,
-				listObjectsV2: 0,
+			calls: TotalCurrent{
+				total: Calls{
+					newFromConfig:      0,
+					listObjectsV2:      0,
+					listObjectsV2Input: 0,
+				},
+				concurrent: Calls{
+					newFromConfig:      0,
+					listObjectsV2:      0,
+					listObjectsV2Input: 0,
+				},
 			},
-			channels: make(chan *s3.Client, s3Worker),
+			channels: make(chan *s3.Client, s3Workers),
+		},
+		aws: *aws.NewConfig(),
+		output: Output{
+			fileStream: os.Stdout,
 		},
 	}
+
+	parseArgs(&app, os.Args)
+	awsCfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(*app.config.region))
+	if err != nil {
+		panic("aws configuration error, " + err.Error())
+	}
+	app.aws = awsCfg
 
 	chstatus := make(chan RunStatus, 100)
 	go func() {
@@ -233,73 +225,49 @@ func main() {
 		lastTotal := uint64(0)
 		for item := range chstatus {
 			total += item.outObjects
-			if lastTotal/2000 != total/2000 {
-				fmt.Fprintf(os.Stderr, "Done=%d ListObjects=%d NewFromConfig=%d\n", total, app.clients.calls.listObjectsV2, app.clients.calls.newFromConfig)
+			if lastTotal/(*app.config.statsFragment) != total/(*app.config.statsFragment) {
+				fmt.Fprintf(os.Stderr, "Done=%d inputConcurrent=%d listObjectsV2=%d/%d listObjectsV2Input=%d/%d NewFromConfig=%d/%d\n",
+					total,
+					app.inputConcurrent,
+					app.clients.calls.total.listObjectsV2,
+					app.clients.calls.concurrent.listObjectsV2,
+					app.clients.calls.total.listObjectsV2Input,
+					app.clients.calls.concurrent.listObjectsV2Input,
+					app.clients.calls.total.newFromConfig,
+					app.clients.calls.concurrent.newFromConfig,
+				)
 				lastTotal = total
 			}
 		}
 	}()
 
-	cho := make(chan Complete, 100)
-	chi := make(chan *s3.ListObjectsV2Input, 2000)
-	// pooli := pond.New(app.config.s3Worker, app.config.s3Worker) //, pond.MinWorkers(app.config.s3Worker))
-	pooli := workerpool.New(app.config.s3Worker)
+	cho := make(chan Complete, *app.config.maxKeys)
+	chi := make(chan *s3.ListObjectsV2Input, (*app.config.maxKeys)*int32(*app.config.s3Workers))
+	pooli := pond.New(*app.config.s3Workers, *app.config.s3Workers)
 	go func() {
 		for item := range chi {
-			// fmt.Fprintln(os.Stderr, "FromChi prefix=", *item.Prefix, " item=", item)
 			citem := *item
 			pooli.Submit(func() {
-				// fmt.Fprintln(os.Stderr, "SubmitChi prefix=", *citem.Prefix, " item=", citem)
 				s3Lister(citem, chi, cho, &app)
 			})
 		}
-		// fmt.Fprintln(os.Stderr, "Exit the Chi")
 	}()
-	// for i := 0; i < 8; i++ {
-	// wgi.Add(i)
-	// go s3Lister(&wgi, chi, cho, cfg, bucket, &delimiter, &prefix, &s3ListerConcurrent)
-	// }
 	atomic.AddInt32(&app.inputConcurrent, 1)
 	chi <- &s3.ListObjectsV2Input{
-		MaxKeys:   app.config.maxKeys,
-		Delimiter: &app.config.delimiter,
-		Prefix:    &app.config.prefix,
-		Bucket:    &app.config.bucket,
+		MaxKeys:   *app.config.maxKeys,
+		Delimiter: app.config.delimiter,
+		Prefix:    app.config.prefix,
+		Bucket:    app.config.bucket,
 	}
-	// poolo := pond.New(app.config.outWorker, 0, pond.MinWorkers(app.config.outWorker))
-	poolo := workerpool.New(app.config.outWorker)
-	poolo.SubmitWait(func() {
+	poolo := pond.New(*app.config.outWorkers, 0, pond.MinWorkers(*app.config.outWorkers))
+	poolo.SubmitAndWait(func() {
 		for items := range cho {
 			if items.completed {
 				fmt.Fprintln(os.Stderr, "Exit-Items", items)
 				return
 			}
-			// fmt.Fprintln(os.Stderr, "Items", items)
-			outWriter(items, chstatus)
+			outWriter(app, items, chstatus)
 		}
 	})
 	fmt.Fprintln(os.Stderr, "Exit")
-
-	// time.Sleep(time.Second * 10)
-
-	// 	observable.w
-	// disposed, cancel := observable.Connect()
-	// go func() {
-	// 	// Do something
-	// 	time.Sleep(time.Second)
-	// 	// Then cancel the subscription
-	// 	cancel()
-	// }()
-	// // Wait for the subscription to be disposed
-	// <-disposed
-	// 	// }()
-
-	// prefix := ""
-	// delimiter := "/"
-	// input := &s3.ListObjectsV2Input{
-	// 	Delimiter: &delimiter,
-	// 	Prefix:    &prefix,
-	// 	Bucket:    bucket,
-	// }
-
 }
