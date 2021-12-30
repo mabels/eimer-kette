fd3f00103db2970ab3b5ad8b5671516e2b700a70
Meno Abels <meno.abels@adviser.com>
2021-12-30 10:58:24 +0100

    chore: missed directory renaming

---
+++ cmd/eimer-kette/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/mabels/eimer-kette/config"
+	"github.com/mabels/eimer-kette/frontend"
+	myq "github.com/mabels/eimer-kette/my-queue"
+	ow "github.com/mabels/eimer-kette/outwriter"
+	"github.com/mabels/eimer-kette/status"
+)
+
+var version = "develop"
+var commit = "unknown"
+
+func main() {
+	config.Version = version
+	config.GitCommit = commit
+	app := config.DefaultS3StreamingLister()
+	config.InitS3StreamingLister(app)
+	if *app.Config.Lambda.Start {
+		lambda.Start(AwsHandleRequest)
+		return
+	}
+
+	chstatus := myq.MakeChannelQueue(*app.Config.Output.Sqs.Workers * *app.Config.S3Workers * 10)
+	cho := ow.OutWriterProcessor(app, chstatus)
+	frontend.Frontend(app, cho, chstatus)
+
+	status.StatusWorker(app, chstatus)
+}
